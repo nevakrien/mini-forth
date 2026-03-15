7efdb4de5b4392224a8b1f5b29c5077d7caf01a8
Neva <nevo.krien@gmail.com>
2026-03-15 18:28:44 +0200

    added a lex

--- compile.h
+++ compile.h
@@ -3,9 +3,19 @@
 
 #include "vm.h"
 #include "basic_ops.h"
+#include "third_party/uthash.h"
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+
+
+
+typedef struct {
+    Comp comp;
+    bool is_inline:1;
+    bool is_now:1;
+} Word;
+
 
 static inline void comp_push_word(Comp* comp,word_t c){
 	ARR_ENSURE_CAP(*comp,comp->len+sizeof(c));
@@ -16,12 +26,6 @@ static inline void comp_push_word(Comp* comp,word_t c){
 static inline void comp_push_code(Comp* comp,code_t c){
 	ARR_PUSH(*comp,c);
 }
-
-typedef struct {
-    Comp comp;
-    bool is_inline:1;
-    bool is_now:1;
-} Word;
 
 static inline void compile_later(Comp* comp, const Word* w){
     if(!w->is_inline){
@@ -42,6 +46,63 @@ static inline void compile_word(VM* vm, const Word* w){
         compile_later(&vm->comp, w);
 }
 
+static inline void* xmalloc(size_t sz) {
+    void* p = malloc(sz);
+    if (!p) abort();
+    return p;
+}
+
+static inline void* xrealloc(void* p, size_t sz) {
+    p = realloc(p, sz);
+    if (!p) abort();
+    return p;
+}
+
+typedef struct LexEntry {
+    char* name;
+    Word* word;
+    UT_hash_handle hh;
+} LexEntry;
+
+typedef struct {
+    LexEntry* entries;
+} Lex;
+
+static inline void destroy_word(Word* word){
+	free(word->comp.data);
+	word->comp.data=NULL;
+}
+
+static inline void lex_init(Lex* lex) {
+    lex->entries = NULL;
+}
+
+static inline void lex_free(Lex* lex) {
+    LexEntry *entry, *tmp;
+    HASH_ITER(hh, lex->entries, entry, tmp) {
+        HASH_DELETE(hh, lex->entries, entry);
+        free(entry->name);
+        destroy_word(entry->word);
+        free(entry->word);
+        free(entry);
+    }
+}
+
+static inline Word* lex_find(Lex* lex, const char* name, size_t name_len) {
+    LexEntry* entry;
+    HASH_FIND(hh, lex->entries, name, name_len, entry);
+    return entry ? entry->word : NULL;
+}
+
+static inline void lex_define(Lex* lex, const char* name, size_t name_len, Word* word) {
+    LexEntry* entry;
+    entry = (LexEntry*)xmalloc(sizeof(LexEntry));
+    entry->name = (char*)xmalloc(name_len + 1);
+    memcpy(entry->name, name, name_len);
+    entry->name[name_len] = '\0';
+    entry->word = word;
+    HASH_ADD_KEYPTR(hh, lex->entries, entry->name, name_len, entry);
+}
 
 #endif // COMPILE_H
 
---
+++ test_lex.c
@@ -0,0 +1,55 @@
+#include "vm.h"
+#include "compile.h"
+#include "basic_ops.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+static void test_lex_last_version_first_large(void){
+	Lex lex;
+	lex_init(&lex);
+
+	const size_t count = 10000;
+	Word* last_inserted[100];
+
+	for(size_t i = 0; i < count; i++){
+		char name[32];
+		snprintf(name, sizeof(name), "key_%zu", i % 100);
+
+		Word* word = (Word*)xmalloc(sizeof(Word));
+		word->comp.data = (code_t*)xmalloc(1);
+		word->comp.data[0] = (code_t)i;
+		word->comp.len = 1;
+		word->is_inline = false;
+		word->is_now = false;
+
+		if(i >= count - 100){
+			last_inserted[i - (count - 100)] = word;
+		}
+
+		lex_define(&lex, name, strlen(name), word);
+	}
+
+	for(size_t i = 0; i < 100; i++){
+		char name[32];
+		snprintf(name, sizeof(name), "key_%zu", i);
+
+		Word* found = lex_find(&lex, name, strlen(name));
+		assert(found != NULL && "key should be found");
+
+		assert(found == last_inserted[i] && "last inserted version should be found first (pointer equality)");
+		printf("key_%zu: found pointer %p == expected %p (value=%d)\n", 
+			i, (void*)found, (void*)last_inserted[i], (int)found->comp.data[0]);
+	}
+
+	printf("test_lex_last_version_first_large: PASSED (count=%zu)\n", count);
+
+	lex_free(&lex);
+}
+
+int main(void){
+	test_lex_last_version_first_large();
+	printf("\nAll tests PASSED!\n");
+	return 0;
+}
